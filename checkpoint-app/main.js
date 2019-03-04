6a781cbbb7e29188ab48433cf75f7ddc62e7ba1f
Jared Smith <j.danielsmith451@gmail.com>
2019-03-04 14:11:36 -0800

    Submission checkpoint 4

---
+++ checkpoint-app/main.js
@@ -0,0 +1,23 @@
+const greetings = require("./greeting.js");
+
+const salutations = require("./salutations.js");
+
+// "Hello"
+greetings.greetingEnglish();
+//Bye bye now//
+salutations.salutationsEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+//Sayanora//
+salutations.salutationsJapanese();
+
+//Hola//
+greetings.greetingSpanish();
+//Adios//
+salutations.salutationsSpanish();
+
+//Halló//
+greetings.greetingIcelandic();
+//Bless//
+salutations.salutationsIcelandic();
---
+++ checkpoint-app/salutations.js
@@ -0,0 +1,15 @@
+exports.salutationsEnglish = function(){
+ console.log("Bye bye now.");
+}
+
+exports.salutationsJapanese = function(){
+ console.log("Sayōnara.");
+}
+
+exports.salutationsSpanish = function(){
+ console.log("Adios.");
+}
+
+exports.salutationsIcelandic = function(){
+ console.log("Bless.");
+}
